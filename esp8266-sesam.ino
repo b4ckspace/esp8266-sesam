20139dc6e5d94af7848c6591051408ab80e10481
Christian Ehehalt <chrisi@chrisi>
2016-05-22 20:12:43 +0200

    Display added, WIFI changed from AP to Client

--- esp8266-sesam.ino
+++ esp8266-sesam.ino
@@ -3,6 +3,8 @@
 #include <ArduinoJson.h>
 #include <ESP8266WebServer.h>
 #include <SimpleTimer.h>
+#include <SSD1306.h>
+#include <SSD1306Ui.h>
 #include "static.h"
 #include "settings.h"
 
@@ -14,6 +16,10 @@ const PROGMEM char text_css[] = "text/css";
 
 ESP8266WebServer server(80);
 SimpleTimer timer;
+WiFiClient wifiClient;
+
+SSD1306   display(0x3c, D3, D4);
+SSD1306Ui ui     ( &display );
 
 typedef struct {
   const char* name;
@@ -23,22 +29,52 @@ typedef struct {
 } t_endpoint;
 
 t_endpoint endpoints[] = {
-  { "Garagentor", "/relais/garageDoor", D5, 1000 },
+  { "Garagentor", "/relais/garageDoor", D7, 1000 },
   { "Haustür", "/relais/frontDoor",  D6, 1000 }
 };
 
 void setup() {
   delay(1000);
   Serial.begin(115200);
+
+  
+  // initialize dispaly
+  display.init();
+  display.clear();
+  display.display();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setContrast(255);
+    
   Serial.println();
   Serial.print("Configuring access point...");
 
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid, password);
 
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+  WiFi.hostname(HOSTNAME_ESP);
+  WiFi.mode(WIFI_MODE);
+
+ 
+  if (WiFi.SSID() != ssid) {
+    WiFi.begin(ssid, password);
+    WiFi.persistent(true);
+    WiFi.setAutoConnect(true);
+    WiFi.setAutoReconnect(true);
+  }
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("\n");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  display.clear();
+  display.drawString(64, 10, "Client running");
+  display.drawString(64, 20, WiFi.localIP().toString());  
+  display.display();
+    
 
   
   server.on("/", []() {
